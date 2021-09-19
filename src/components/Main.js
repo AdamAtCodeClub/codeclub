4296222ba1324f07116879c87ab8c7118fd208a2
AdamAtCodeClub <adam@codeclub.im>
2021-09-19 16:51:58 +0100

    change routing

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,26 +2,31 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from './pages/Home';
 
-import Coding_Resources from './pages/Learn_To_Code/Coding_Resources';
-import Whats_On from './pages/Learn_To_Code/Whats_On';
+import CodingResources from './pages/LearnToCode/CodingResources';
+import WhatsOn from './pages/LearnToCode/WhatsOn';
 
-import Our_Projects from './pages/Help_Us_Grow/Our_Projects';
-import Volunteer from './pages/Help_Us_Grow/Volunteer';
+import OurProjects from './pages/HelpUsGrow/OurProjects';
+import Volunteer from './pages/HelpUsGrow/Volunteer';
 
-import Our_Mission from "./pages/Our_Mission";
+import OurMission from "./pages/OurMission";
+
+import Unknown from "./pages/404";
 
 const Main = () => {
     return (
         <Switch>
             <Route exact path ='/' component={Home}></Route>
+            
 
-            <Route exact path = '/coding-resources' component={Coding_Resources}></Route>
-            <Route exact path = '/whats-on' component={Whats_On}></Route>
+            <Route exact path = '/coding-resources' component={CodingResources}></Route>
+            <Route exact path = '/whats-on' component={WhatsOn}></Route>
 
-            <Route exact path = '/our-projects' component={Our_Projects}></Route>
+            <Route exact path = '/our-projects' component={OurProjects}></Route>
             <Route exact path = '/volunteer' component={Volunteer}></Route>
 
-            <Route exact path = '/our-mission' component={Our_Mission}></Route>
+            <Route exact path = '/our-mission' component={OurMission}></Route>
+            
+            <Route exact path = '*' component={Unknown}></Route>
         </Switch>
     )
 }
---
+++ src/components/pages/404.js
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap"
+
+import './Home.css';
+
+export default function Home(){
+    return (
+        <Container>
+            <h1>404. Something's gone wrong, check your link.</h1>
+        </Container>
+    )
+}
